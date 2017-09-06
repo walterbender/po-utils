4d98b0378a16f1249fd6e366ecefc7ada1ac7209
eohomegrownapps <euan.l.y.ong@gmail.com>
2017-09-06 16:21:09 +0100

    Added unicode support for pot2po.py
    As in title

--- pot2po.py
+++ pot2po.py
@@ -10,12 +10,13 @@
 import os
 import re
 import sys
+import codecs
 
 
 def convert_pot_to_po(pot_filename, po_filename, trans_filename):
 
     # Read data from the existing POT file
-    pot_fd = open(pot_filename, 'r')
+    pot_fd = codecs.open(pot_filename, 'r', 'UTF-8')
     pot_list = []
 
     trans_note = []
@@ -47,7 +48,7 @@ def convert_pot_to_po(pot_filename, po_filename, trans_filename):
     if trans_filename == '':
         trans_dict = {}
     else:
-        trans_fd = open(trans_filename, 'r')
+        trans_fd = codecs.open(trans_filename, 'r', 'UTF-8')
 
         trans_dict = {}
 
@@ -63,7 +64,7 @@ def convert_pot_to_po(pot_filename, po_filename, trans_filename):
 
         trans_fd.close()
 
-    po_fd = open(po_filename, 'r')
+    po_fd = codecs.open(po_filename, 'r', 'UTF-8')
     po_dict = {}
     po_header = ''
 
@@ -123,7 +124,7 @@ def convert_pot_to_po(pot_filename, po_filename, trans_filename):
 
     po_fd.close()
 
-    output = open(po_filename + '_', 'w')
+    output = codecs.open(po_filename + '_', 'w', 'UTF-8')
     output.write(po_header)
 
     for i in range(len(pot_list)):
